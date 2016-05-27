e4a1c2e2b3b80479cf1f9d278a1eac2c92582a9b
lonesentinel19 <racingfan14@frontier.com>
2016-05-27 16:18:00 -0400

    add example config

---
+++ config.example.js
@@ -0,0 +1,9 @@
+var mapLoc = "img/mymap.png";
+var scale = .8382; // 1 px = scale(cm) (e.g: a value of .8382 means that one pixel on the screen equals 1 cm)
+var maxZoom = 2.0;
+var minZoom = 0.5;
+var height = 492; // in pixels, original size
+var width = 992; // in pixels, original size
+var locations = {
+	'park': [0, 44, 229, 344, "A cool park with trees."]
+	}
